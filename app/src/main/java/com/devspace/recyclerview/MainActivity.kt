96f1af963692f7412ff50bb36f8f661a22976bf6
ddoniela <sousa.danielalima@gmail.com>
2024-11-26 14:01:24 -0300

    implementing recycler view and adapter

--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +17,25 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val rvList = findViewById<RecyclerView>(R.id.rv_list)
     }
 }
+
+val contact = listOf(
+    Contact("Dani", "1234567890", R.drawable.sample1),
+    Contact("Eduardo", "12393839", R.drawable.sample2),
+    Contact("Paola", "4343567890", R.drawable.sample3),
+    Contact("Pablo", "21323123", R.drawable.sample4),
+    Contact("Joana", "454354444", R.drawable.sample5),
+    Contact("Lucas", "64565464", R.drawable.sample6),
+    Contact("Renata", "76876876876", R.drawable.sample7),
+    Contact("Abílio", "423423432", R.drawable.sample8),
+    Contact("Ingrid", "5464352342", R.drawable.sample9),
+    Contact("Caio", "654765867867", R.drawable.sample10),
+    Contact("Maiara", "5342543643", R.drawable.sample11),
+    Contact("Arthur", "32323232", R.drawable.sample12),
+    Contact("Camila", "45364353464", R.drawable.sample13),
+    Contact("Rodrigo", "4243254654654", R.drawable.sample14),
+    Contact("Lara", "43654657676", R.drawable.sample15),
+)
